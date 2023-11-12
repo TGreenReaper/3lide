04f7dc50f45d987f271686e1c5aad14cce5dcc2a
TGreenReaper <140297612+TGreenReaper@users.noreply.github.com>
2023-11-12 15:59:26 -0600

    better

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let lang = "jquery";
 document.addEventListener("DOMContentLoaded"  , function(){
+  document.getElementById("using").innerHTML = "You are using: " + lang
   document.getElementById("run").addEventListener("click" , function(){
     if(lang == "jquery"){
-        var scr = document.createElement("script");
+       var scr = document.createElement("script");
        scr.innerHTML = document.getElementById('txt').value
        document.getElementById("output").appendChild(scr)
        scr.remove()
@@ -22,15 +23,61 @@ document.addEventListener("DOMContentLoaded"  , function(){
     if(event.keyCode == 13){
         if( document.getElementById("txt").value == "/html"){
             lang = "html"
-            alert("HTML")
+            document.getElementById("using").innerHTML = "You are using: " + lang
+            document.getElementById("noti").style.display = "flex";
+            document.getElementById("notif_txt").innerHTML = "HTML Selected"
+            document.getElementById("noti").style.color = "brown"
+            document.getElementById("noti").style.borderColor = "brown"
+            document.getElementById("notif_txt").style.borderBottomColor = "brown"
+            setTimeout(function(){
+              document.getElementById("noti").style.display = "none"
+            }, 1500)
+
         }
 
         else if(document.getElementById("txt").value == "/css"){
             lang = "css";
-            alert("CSS")
+            document.getElementById("using").innerHTML = "You are using: " + lang
+            document.getElementById("noti").style.display = "flex";
+            document.getElementById("notif_txt").innerHTML = "{CSS: Selected;}"
+            document.getElementById("noti").style.color = "blue"
+            document.getElementById("noti").style.borderColor = "blue"
+            document.getElementById("notif_txt").style.borderBottomColor = "blue"
+
+
+            setTimeout(function(){
+              document.getElementById("noti").style.display = "none"
+            }, 1500)
+
         }
 
-        else if(document.getElementById("txt").value == "/jQuery" || document.getElementById("txt").value == "/jquery" ){}
-    }
+        else if(document.getElementById("txt").value == "/help"){
+          document.getElementById("txt").value = "/jquery (Switches to jquery, it is used by default\n/html (Switches to HTML)\n/css (You totally do not know what this will do.)";
+          setTimeout(function() {
+            document.getElementById("txt").value = "";
+          } , 6000)
+        }
+
+        else if(document.getElementById("txt").value == "/jQuery" || document.getElementById("txt").value == "/jquery" ){
+          lang = "jquery";
+          document.getElementById("using").innerHTML = "You are using: " + lang
+          var scr = document.createElement("script");
+          scr.innerHTML = document.getElementById('txt').value
+          document.getElementById("output").appendChild(scr)
+          scr.remove()
+          document.getElementById("noti").style.display = "flex";
+          document.getElementById("notif_txt").innerHTML = "$(jQuery).Selected"
+          document.getElementById("noti").style.color = "green"
+          document.getElementById("noti").style.borderColor = "green"
+          document.getElementById("notif_txt").style.borderBottomColor = "green"
+
+       setTimeout(function(){
+         document.getElementById("noti").style.display = "none"
+       }, 1500)
+
+        } 
+    } 
   })
+  
 })
+
